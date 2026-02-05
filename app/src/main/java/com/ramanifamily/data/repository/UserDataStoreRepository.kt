2eecfc6acdc459e058101d2888f9e7b0d631e9a5
Shailesh K Limbadiya <limbadiya.sk@gmail.com>
2026-02-05 13:06:31 +0530

    Update UserDataStoreRepository.kt

--- app/src/main/java/com/ramanifamily/data/repository/UserDataStoreRepository.kt
+++ app/src/main/java/com/ramanifamily/data/repository/UserDataStoreRepository.kt
@@ -3,6 +3,7 @@ package com.ramanifamily.data.repository
 import android.content.Context
 import com.ramanifamily.data.datastore.loginResponseDataStore
 import com.ramanifamily.data.entity.LoginResponse
+import com.ramanifamily.data.entity.ProfileResponse
 import com.ramanifamily.data.datastore.mapper.toProto
 import com.ramanifamily.datastore.LoginResponseProto
 import kotlinx.coroutines.flow.Flow
@@ -13,6 +14,14 @@ class UserDataStoreRepository (private val context: Context) {
     suspend fun saveLoginResponse(response: LoginResponse) {
         context.loginResponseDataStore.updateData {
             response.toProto()
+        }
+    }
+
+    suspend fun saveProfileResponse(response: ProfileResponse) {
+        context.loginResponseDataStore.updateData { currentProto ->
+            currentProto.toBuilder()
+                .setUser(response.data.toProto())
+                .build()
         }
     }
 
